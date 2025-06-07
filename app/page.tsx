1b6b707435592bb1a8b9cd4bd2ac3a4308668031
Elijah Gummer <elijahgummer5@gmail.com>
2025-06-07 20:53:04 +1000

    changed the code

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import Image from 'next/image';
 import ResumeAnalyzerApp from './components/ResumeAnalyzerApp';
-import styles from './styles/Home.module.css';
 
 export default function Home() {
   return (
